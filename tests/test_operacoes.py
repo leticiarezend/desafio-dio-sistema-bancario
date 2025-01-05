f99723bc9ff00c5d653d263a56712fab00846088
Letícia Rezende <leticia.rezende1020@outlook.com>
2025-01-05 14:59:38 -0300

    chore: cria teste para depósito negativo

--- tests/test_operacoes.py
+++ tests/test_operacoes.py
@@ -8,6 +8,11 @@ class TestContaBancaria(unittest.TestCase):
         conta.depositar(100)
         self.assertEqual(conta.get_saldo(), 100)
 
+    def test_deposito_negativo(self):
+        conta = ContaBancaria()
+        conta.depositar(-50)
+        self.assertEqual(conta.get_saldo(), 0)
+
 
 if __name__ == "__main__":
     unittest.main()
