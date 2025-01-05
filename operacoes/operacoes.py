0573209475aef2732e50e09c2587f2ff7aab34aa
Letícia Rezende <leticia.rezende1020@outlook.com>
2025-01-05 15:19:18 -0300

    feat: cria método de registro de transação e aplica no deposito

--- operacoes/operacoes.py
+++ operacoes/operacoes.py
@@ -9,8 +9,12 @@ class ContaBancaria:
             if deposito <= 0:
                 raise ValueError("Depósito deve ser maior que 0!")
             self._saldo += deposito
+            self.registrar_transacao({"tipo": "Depósito", "valor": deposito})
         except ValueError as e:
             return e
+
+    def registrar_transacao(self, transacao):
+        self._transacoes.append(transacao)
 
     def get_saldo(self):
         return self._saldo
