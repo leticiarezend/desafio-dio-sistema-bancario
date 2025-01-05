784dd7b8998e2d9d784c355640323de4875cbd1f
Letícia Rezende <leticia.rezende1020@outlook.com>
2025-01-05 15:53:42 -0300

    chore: cria teste de saque negativo

--- tests/test_operacoes.py
+++ tests/test_operacoes.py
@@ -18,6 +18,13 @@ class TestContaBancaria(unittest.TestCase):
         conta.sacar(20)
         self.assertEqual(conta.get_saldo(), 30)
 
+    def test_saque_negativo(self):
+        conta = ContaBancaria(100)
+        with self.assertRaises(ValueError) as context:
+            conta.sacar(-50)
+        self.assertEqual(str(context.exception), "Valor do saque deve ser maior que 0!")
+        self.assertEqual(conta.get_saldo(), 100)
+
 
 if __name__ == "__main__":
     unittest.main()
