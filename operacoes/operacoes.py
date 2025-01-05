0f3610d290bdb18896e924baaba3a617647a12a8
Letícia Rezende <leticia.rezende1020@outlook.com>
2025-01-05 15:56:43 -0300

    feat: cria método de saque

--- operacoes/operacoes.py
+++ operacoes/operacoes.py
@@ -3,6 +3,7 @@ class ContaBancaria:
         self._saldo = saldo_inical
         self._transacoes = []
         self._saques = 0
+        self.limite_saques = 3
 
     def depositar(self, deposito):
         try:
@@ -15,6 +16,23 @@ class ContaBancaria:
 
     def registrar_transacao(self, transacao):
         self._transacoes.append(transacao)
+
+    def sacar(self, saque):
+        if saque <= 0:
+            raise ValueError("Valor do saque deve ser maior que 0!")
+        if saque >= 500:
+            raise ValueError(
+                "Valor limite de saque de R$ 500,00 excedido. Saque um valor inferior"
+            )
+        if saque > self._saldo:
+            raise ValueError(
+                f"Saldo insuficiente para saque de {saque}. Seu saldo atual é de {self._saldo}"
+            )
+        if self.limite_saques <= 0:
+            raise ValueError("Limite diário de 3 saques atingido.")
+        self._saldo -= saque
+        self.registrar_transacao({"tipo": "Saque", "valor": saque})
+        self.limite_saques -= 1
 
     def get_saldo(self):
         return self._saldo
